51fcda7eaba51f66c50cbb38f89d3ed9ec1fd3ab
abhishek <jainabhishek347@gmail.com>
2022-07-08 11:35:10 +0530

    temp commit 16

--- lambdas/etl/etl.py
+++ lambdas/etl/etl.py
@@ -6,7 +6,7 @@ import psycopg2
 import numpy as np
 import psycopg2.extras as extras
 
-import extract, load_db, load_basket, table_script
+# import extract, load_db, load_basket, table_script
 from db_utils import get_db_connection
 
 LOGGER = logging.getLogger()
@@ -15,6 +15,52 @@ FIELDNAMES =['timestamp','store','customer_name',
              'basket_items','total_price','cash_or_card','card_number']
 
 FILENAME = 'chesterfield.csv'
+
+
+def db_create_tables(conn, cur):    
+    ## create customer table ## as ** customer_df
+    create_customer_table = '''CREATE TABLE IF NOT EXISTS customer_df(
+                            customer_id     INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                            customer_name   TEXT,
+                            card_number     text                          
+                            );'''
+    
+    ## create store table ## as ** store_df
+    create_store_table = '''CREATE TABLE IF NOT EXISTS  store_df(
+                            store_id     INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                            store        TEXT
+                            );'''
+    
+    ## create basket table ## as ** basket_df                     
+    create_basket_table = '''CREATE TABLE IF NOT EXISTS  basket_df(
+                            order_id         integer,
+                            product_id       integer,
+                            customer_id      integer,
+                            store_id         integer,
+                            time_stamp       text,
+                            constraint fk_product
+                                foreign key (product_id) 
+                                REFERENCES products_df (product_id),
+                            constraint fk_customer
+                                foreign key (customer_id) 
+                                REFERENCES customer_df (customer_id),
+                            constraint fk_store
+                                foreign key (store_id) 
+                                REFERENCES store_df (store_id)
+                            );'''
+    
+    ## create products table ## as ** basket_df                     
+    create_products_table = '''CREATE TABLE IF NOT EXISTS products_df(
+                            product_id      INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                            product_name    TEXT,
+                            product_flavour    TEXT,    
+                            product_price    TEXT
+                            );'''
+    
+    # executing tables 
+    cur.execute(f' {create_customer_table}{create_store_table}{create_products_table}{create_basket_table}')
+    print('tables have been created!!')
+    conn.commit()
 
 
 def csv_to_df(DATA):
@@ -36,6 +82,9 @@ def lambda_handler(event, context):
 
     print('=============================')
     print(event)
+
+    conn, cursor = get_db_connection()
+    db_create_tables(conn, cur)
 
     bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
     s3_file_name = event["Records"][0]["s3"]["object"]["key"]
